2b0372a230166ff5a3fba373d89aba3b06e513bf
Stevens Bougault <bougault.stevens@gmail.com>
2024-01-26 06:02:51 +0000

    Navbar code improvement changed px to rem, changed paddings, and added comments

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react";
-import { animated, useSpring } from "react-spring";
-import nouxLogo from "../assets/images/NouxLogo.svg?react";
-import { navigationLinks } from "../constants";
-import { NavBarMenu } from "../components";
-import { Link } from "react-router-dom";
+import { animated, useSpring } from "react-spring"; // For the removal of the navbar container when scrolling
+import { navigationLinks } from "../constants"; // Links for the navbar and the side menu
+import { NavBarMenu } from "../components"; // Side menu
+import { Link } from "react-router-dom"; // For the rounting of the links
+import { ReactSVG } from "react-svg"; // Import SVG
 
 const Navbar = () => {
+  // --------------------------------------------------------------------------------------------------------------------------------
+  // NAVBAR ANIMATION WHEN SCROLLING
+
+  // Keep track of the removal of the navbar container when scrolling
   const [removeComponents, setRemoveComponents] = useState(false);
 
   // Define spring animation for removal of the navbar container when scrolling
   const removalSpring = useSpring({
     opacity: removeComponents ? 0 : 1,
-    transform: removeComponents ? "translateY(-50px)" : "translateY(0)",
-    config: { duration: 300 }, // Adjust duration
+    transform: removeComponents ? "translateY(-3.125)" : "translateY(0)",
+    config: { duration: 300 },
   });
+
   // Define spring animation for removal of the navbar container when scrolling
   useEffect(() => {
     let prevScrollY = window.scrollY;
 
     const handleScroll = () => {
+      // When the user scrolls down 30px from the top effect start taking place
       if (window.scrollY > 30) {
         const currentScrollY = window.scrollY;
 
@@ -39,14 +45,17 @@ const Navbar = () => {
 
     window.addEventListener("scroll", handleScroll);
 
+    // Remove the event listener when the component is unmounted
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  // --------------------------------------------------------------------------------------------------------------------------------
+  // SIDE MENU ANIMATION AND LOGIC WHEN HAMBURGER IS PRESSED
 
   //Keep track of the menu state
   const [isMenuExpanded, setIsMenuExpanded] = useState(false);
-  //Keep track of the link hovered
+  //Keep track of the link hovered in the side menu
   const [currentIndex, setCurrentIndex] = useState(0);
 
   // set the menu state to the opposite of what it is
@@ -78,13 +87,13 @@ const Navbar = () => {
         style={removalSpring}
         className={`fixed left-0 top-0 right-0 z-10`}
       >
-        <section className="px-4 mt-4">
-          <nav className="flex justify-between items-center rounded-[3rem] h-[72px] bg-[#F6F1FD] pl-4 pr-2 text-2xl font-normal max-w-[112.5rem] mx-auto">
+        <section className="mt-4 mx-8">
+          <nav className="flex justify-between items-center rounded-[3rem] h-[4.5rem] bg-[#F6F1FD] pl-6 pr-2 text-2xl font-normal max-w-[112.5rem] mx-auto">
             <div
               className={`${
                 isMenuExpanded
-                  ? "[clip-path:circle(5000px_at_100%_-15%)]"
-                  : "[clip-path:circle(0px_at_100%_-15%)]"
+                  ? "[clip-path:circle(250rem_at_100%_-15%)]"
+                  : "[clip-path:circle(0rem_at_100%_-15%)]"
               } z-20 absolute top-0 left-0 right-0 bg-[#110326] transition-all transform duration-1000 flex justify-center items-center`}
               //   Inline css to remove transition on resizing window to affect only the height
               style={{
@@ -95,7 +104,7 @@ const Navbar = () => {
               <NavBarMenu />
             </div>
             <Link to="/">
-              <img src={nouxLogo} alt="" />
+              <ReactSVG src="src/assets/images/nouxlogo.svg" />
             </Link>
             <ul className={"flex gap-8"}>{renderNavbarLinks()}</ul>
             <div>
@@ -107,14 +116,14 @@ const Navbar = () => {
               >
                 <div className="flex flex-col">
                   <div
-                    className={`bg-black h-[4px] w-[32px] group-duration-4000 my-[0.188rem] transition-all transform duration-1000 rounded-3xl ${
+                    className={`bg-black h-[0.25rem] w-[2rem] group-duration-4000 my-[0.188rem] transition-all transform duration-1000 rounded-3xl ${
                       isMenuExpanded
                         ? "bg-white rotate-45 translate-y-[0.3rem]"
                         : ""
                     }`}
                   />
                   <div
-                    className={`bg-black h-[4px] w-[32px] my-[0.188rem] transition-all transform duration-1000 rounded-3xl ${
+                    className={`bg-black h-[0.25rem] w-[2rem] my-[0.188rem] transition-all transform duration-1000 rounded-3xl ${
                       isMenuExpanded
                         ? "bg-white -rotate-45 -translate-y-[0.3rem]"
                         : ""
